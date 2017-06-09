5898824236bf81a72c0641c27901a325eb45800d
matikun86 <matiasfarulla@gmail.com>
2017-06-10 11:18:39 +1200

    Added some content

---
+++ src/index.js
@@ -0,0 +1,3 @@
+require('./styles/index.scss');
+require("file-loader?name=[name].[ext]!./index.html");
+require("file-loader?name=contenidos/[name].[ext]!./contenidos/index.html");
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    path.resolve(__dirname, 'src/index.js')
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, loaders: ["style-loader", "css-loader"] },
+      { test: /\.scss$/, loader: ["style-loader", "css-loader", "sass-loader"] }
+    ]
+  }
+};
