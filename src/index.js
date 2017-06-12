12334e25773beccae4709cb67ce24504bb1ce52e
matikun86 <matiasfarulla@gmail.com>
2017-06-12 22:16:25 +1200

    build

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 require('./styles/index.scss');
 require("file-loader?name=[name].[ext]!./index.html");
 require("file-loader?name=contenidos/[name].[ext]!./contenidos/introduccion.html");
-require("file-loader?name=contenidos/[name].[ext]!./contenidos/ejemplos.html");
